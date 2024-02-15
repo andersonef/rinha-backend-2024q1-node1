65a3595d62e9caf28adb3f75a7c1ef82aa809368
Anderson Nunes <anderson.nuneseth@gmail.com>
2024-02-15 00:55:44 -0300

    feat: Using redis to validate balance

--- src/database/repository.js
+++ src/database/repository.js
@@ -4,6 +4,7 @@ const config = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     user: process.env.DB_USER || 'postgres',
+    database: process.env.DB_NAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     max: process.env.MAX_CONNECTION_POOL || 20,
     idleTimeoutMillis: process.env.DB_MAX_IDLE_TIMEOUT || 30000,
--- src/validators/transacao-validator.js
+++ src/validators/transacao-validator.js
@@ -1,15 +1,23 @@
 const CACHE_SALDOS = []
 const db = require('../database/repository')
+const Redis = require('ioredis')
 
+const redis = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379
+
+})
 module.exports = async function transacaoValidator(cliente_id, valor, tipo) {
-    let saldo_cache = CACHE_SALDOS.find(saldo => saldo.cliente_id === cliente_id)
+    let saldo_cache = await redis.get(`saldo:${cliente_id}`)
+    saldo_cache = JSON.parse(saldo_cache)
+
     if (!saldo_cache) {
         saldo_cache = await db.query(
             'select * from clientes where id = $1',
             [cliente_id]
         )
         saldo_cache = saldo_cache.rows[0]
-        CACHE_SALDOS.push(saldo_cache)
+        await redis.set(`saldo:${cliente_id}`, JSON.stringify(saldo_cache))
     }
 
     if (tipo === 'c') {
